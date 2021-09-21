3d1ecf58fe444c0f37f8b48213df77dc87131fb7
Tyler Eckstein <tylereckstein@Tylers-MacBook-Pro.local>
2021-09-21 00:07:13 -0400

    Initial create-react-app/changeBG commit

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { changeColor } from './main';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={changeColor}>
+          Change color
+        </button>
       </header>
     </div>
   );
